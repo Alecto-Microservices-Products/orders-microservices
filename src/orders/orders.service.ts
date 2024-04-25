0d6b296fed4932434228cf39231877e96a392ade
Harry Ore Lara <harryorelara@gmail.com>
2024-04-25 12:47:45 -0500

    Modiicando la linea de obtener todos los productos con paginacion

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -2,6 +2,7 @@ import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { PrismaClient } from '@prisma/client';
 import { RpcException } from '@nestjs/microservices';
+import { OrderPaginationDto } from './dto/order-pagination.dto';
 
 @Injectable()
 export class OrdersService extends PrismaClient implements OnModuleInit {
@@ -18,8 +19,40 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
     });
   }
 
-  findAll() {
-    return `This action returns all orders`;
+  async findAll(orderPaginationDto: OrderPaginationDto) {
+    const totalPages = await this.order.count({
+      //Cuenta el numero total de ordenes
+      where: {
+        status: orderPaginationDto.status, //Cuando el estado cumpla
+      },
+    });
+
+    const currenPage = orderPaginationDto.page; //Page por defecto es 1
+    const perPage = orderPaginationDto.limit; //Limite por defecto es 10
+
+    // Si currentPage = 3 y perPage = 4, entonces cada página mostrará 4 elementos.
+
+    // skip: (currentPage - 1) * perPage: En este caso, (3 - 1) * 4 = 8. Esto significa que se
+    // omitirán los primeros 8 registros de la consulta, ya que estamos en la tercera página y
+    // queremos comenzar desde el noveno elemento.
+    // take: perPage: Tomaremos 4 registros de la base de datos, ya que perPage es 4.
+    // where: { status: orderPaginationDto.status }: Se filtran los registros según el estado
+    // de la orden proporcionado en orderPaginationDto.status.
+
+    return {
+      data: await this.order.findMany({
+        skip: (currenPage - 1) * perPage,
+        take: perPage,
+        where: {
+          status: orderPaginationDto.status,
+        },
+      }),
+      meta: {
+        total: totalPages,
+        page: currenPage,
+        lastPage: Math.ceil(totalPages / perPage),
+      },
+    };
   }
 
   async findOne(id: string) {
