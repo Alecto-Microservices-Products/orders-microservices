c8b574878ee68e2edc042c1640f3fbd0f1deed46
Harry Ore Lara <harryorelara@gmail.com>
2024-04-25 20:01:15 -0500

    Termino de ordenes con todos sus metodos. Antes de NATS

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,22 +1,29 @@
-import 'dotenv/config'
-import * as joi from 'joi'
+import 'dotenv/config';
+import * as joi from 'joi';
 interface EnvVars {
-    PORT: number;
-
+  PORT: number;
+  PRODUCTS_MICROSERVICE_HOST: string;
+  PRODUCTS_MICROSERVICE_PORT: number;
 }
 
-const envSchema = joi.object({
-    PORT: joi.number().required()
-}).unknown(true);
+const envSchema = joi
+  .object({
+    PORT: joi.number().required(),
+    PRODUCTS_MICROSERVICE_HOST: joi.string().required(),
+    PRODUCTS_MICROSERVICE_PORT: joi.number().required(),
+  })
+  .unknown(true);
 
 const { error, value } = envSchema.validate(process.env);
 
 if (error) {
-    throw new Error(`Config validation error: ${error.message}`);
+  throw new Error(`Config validation error: ${error.message}`);
 }
 
 const envVars: EnvVars = value;
 
 export const envs = {
-    port: envVars.PORT
+  port: envVars.PORT,
+  productMicroserviceHost: envVars.PRODUCTS_MICROSERVICE_HOST,
+  productMicroservicePort: envVars.PRODUCTS_MICROSERVICE_PORT,
 };
